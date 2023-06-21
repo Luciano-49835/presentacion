7ebe8f8afc56bdfedcd152cbe97e3b07e40274ee
Luciano-49835 <124514285+Luciano-49835@users.noreply.github.com>
2023-06-20 22:25:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const txt = document.querySelector('#txt');
+
+const img = document.querySelector('#imagen');
+
+let rap = 'This is one of the most important things when we talk about Hip-hop. Rap is the music style with strongs rhythm whose words are not sung, are spoken';
+
+let imagen1 = '<img src=IMG/raperos.jpg>'
+
+let turntablism = 'Nowadays is well known as DJ and this is about creating new music, sounds effects, mixes and creatives sounds and beats';
+
+let imagen2= '<img src=IMG/turntablism.jpg>'
+
+let breaking = 'It´s began in the 1970s and it is the first dance of the street culture of hip-hop and it´s dynamic, acrobatic street-born dance performed by individuals dancers.';
+
+let imagen3 = '<img src=IMG/Break-dance.jpg>'
+
+let graffiti = 'It´s considered as form of visual comunication and in many countries is illegal. The most common image of graffiti is a stylistic symbol or phrase spray-painted. Graffiti is an activity practised by young people.';
+
+let imagen4 = '<img src= IMG/grafiti.jpg>'
+
+//Imprimimos en el span
+
+txt.innerText = '';
+
+img.innerHTML = '';
+
+
+
---
+++ index2.js
@@ -0,0 +1,29 @@
+function texto1(){
+
+    document.querySelector('#texto1').style.display = 'block'
+
+}
+
+function texto2(){
+
+    document.querySelector('#texto2').style.display = 'block'
+
+}
+
+function texto3(){
+
+    document.querySelector('#texto3').style.display = 'block'
+
+}
+
+function texto4(){
+
+    document.querySelector('#texto4').style.display = 'block'
+
+}
+
+function texto5(){
+
+    document.querySelector('#texto5').style.display = 'block'
+
+}
